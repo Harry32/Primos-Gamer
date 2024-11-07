fe1781229be824de8c489eae96c12ec6fcfb7929
Cclezar <clezarcarol@gmail.com>
2024-11-06 22:38:19 -0300

    primeiro arquivo html

--- primos_gamer/produtos/admin.py
+++ primos_gamer/produtos/admin.py
@@ -15,7 +15,7 @@ class TipoProdutoAdmin(admin.ModelAdmin):
 
 @admin.register(Produto)
 class ProdutoAdmin(admin.ModelAdmin):
-    list_display = ['nome', 'tipo', 'categoria', 'data_cadastro', 'preco', 'ativo']
+    list_display = ['nome', 'tipo', 'categoria', 'data_cadastro', 'preco', 'desconto', 'ativo']
     list_filter = ['tipo', 'categoria', ('data_cadastro', admin.DateFieldListFilter), 'ativo']
     # list_per_page = 5
 
@@ -23,7 +23,7 @@ class ProdutoAdmin(admin.ModelAdmin):
     fieldsets = [(None, { 'fields': ['nome', 'categoria', 'tipo', 'preco'] }),
                  ('Detalhes', { 'fields': ['fabricante', 'modelo', 'descricao', 'especificacoes'] })]
     
-    actions = ['desativar']
+    actions = ['desativar', 'ativar', 'descontoBlack']
     
     def has_delete_permission(self, request: HttpRequest, obj: Any | None = ...) -> bool:
         """
@@ -44,4 +44,18 @@ class ProdutoAdmin(admin.ModelAdmin):
             Função criada por mim para uma Action customizada que desativa produtos.
         """
         queryset.update(ativo=False)
-    
+
+    @admin.action(description='Ativar produtos selecionados')
+    def ativar(self, request: HttpRequest, queryset: QuerySet):
+        """
+            Função criada para uma Action customizada que ativa produtos.
+        """
+        queryset.update(ativo=True)    
+
+    @admin.action(description='Descontos Black Friday')
+    def descontoBlack(self, request: HttpRequest, queryset: QuerySet):
+        queryset.filter(preco__gt=2000).update(desconto=20)
+        queryset.filter(preco__gte=1000, preco__lt=2000).update(desconto=35)
+        queryset.filter(preco__gte=500, preco__lt=1000).update(desconto=45)
+            
+            
--- primos_gamer/produtos/views.py
+++ primos_gamer/produtos/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from produtos.models import Categoria
 
-# Create your views here.
+
+def index(request):
+    lista_categorias = Categoria.objects.all()
+    context = {
+        'categorias': lista_categorias
+    }
+
+    return render(request, 'index.html', context)
+
+def detail(request, id_categoria):
+    categoria = Categoria.objects.get(id=id_categoria)
+    context = {
+        'categoria': categoria
+    }
+
+    return render(request, 'detail.html', context)
